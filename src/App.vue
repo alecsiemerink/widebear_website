72e6381ad5e90eade8121399d50e0fae74c1a8a9
Alec Siemerink <alecsiemerink@gmail.com>
2021-05-25 14:39:33 +0200

    Website 1.0
    First design of the onepager website of the Wide Bear Technology project.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="De Oosterhof" src="./assets/logo.png" width="300" heigth="width">
+    <Main msg="Wide Bear"/>
+
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Main
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+    <h3>
+      Technology
+    </h3>
+    <div class="overiglinks">
+        <li>
+          <ul><b><p>Github:</p></b></ul>
+          <ul><a href="https://github.com/alecsiemerink"><p>Alec Siemerink</p></a></ul>
+        </li>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+h3 {
+  font-family: 'Source Code Pro Light', monospace;
+  font-size: 30px;
+}
+h1 {
+  font-family: 'Source Code Pro', monospace;
+  font-size: 55px;
+}
+p {
+  font-family: 'Source Code Pro', monospace;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #2e7aa3;
+}
+ul {
+  display: inline-block;
+  padding: 20px;
+}
+</style>
